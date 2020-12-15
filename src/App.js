8c640867b05f67ad54c6b18cef694ac34e914645
Razvan Grigore <razvang95@gmail.com>
2020-12-15 15:13:39 +0200

    Temporary navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+
+import QuizPage from './components/quiz/QuizPage';
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+	return (
+		<div>
+			<BrowserRouter>
+				<Link to="/quiz">random</Link><br />{/* TODO Remove this */}
+				<Link to={{ pathname: "/quiz", state: { category: 1 } }}>category</Link>{/* TODO Remove this */}
+
+				<Switch>
+					<Route path="/quiz" component={ QuizPage } />
+				</Switch>
+			</BrowserRouter>
+		</div>
+	)
 }
 
 export default App;
