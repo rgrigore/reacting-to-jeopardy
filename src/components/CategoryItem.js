e89d3d1cd82bcba5291603caadc655c4cc010e5e
Dani <daniela.joian@gmail.com>
2020-12-17 09:28:54 +0200

    Link Style

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -10,7 +10,7 @@ const CategoryItem = ({ categories, loading}) => {
             {categories.map(category => (
                 <div key={category.id} className="card">
                     <div className="card-body">
-                        <h5 className="card-title"><a href="/#" >{category.title}</a></h5>
+                        <h5 className="card-title"><a href="/#" style={linkStyle}>{category.title}</a></h5>
                             <p className="card-text">No. of questions: 
                                 <span className="badge badge-info" style={{ color: 'blue'}}>
                                 {category.clues_count}
@@ -22,5 +22,11 @@ const CategoryItem = ({ categories, loading}) => {
         </div>
     );
 };
+
+const linkStyle = {
+    color: '#f2222',
+    textDecoration: 'none'
+}
+
 
 export default CategoryItem;
